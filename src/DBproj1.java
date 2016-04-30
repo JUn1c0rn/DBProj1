ad5bcfbe85b481ee924a328858904679e9605177
JUn1c0rn <JUnicorn@wangjuedeMacBook-Pro.local>
2016-04-30 11:19:59 -0400

    wj

--- src/DBproj1.java
+++ src/DBproj1.java
@@ -1,7 +1,12 @@
-import java.io.BufferedReader;  
-import java.io.FileInputStream;  
-import java.io.InputStreamReader;  
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DBproj1 {
 	public static void main(String[] args){
@@ -11,8 +16,8 @@ public class DBproj1 {
 		}
 	}
 	
-	private void init(){//initialize
-		
+	private void init(String fname){//initialize
+		List<String> dataList=new ArrayList<String>();
 	}
 	
 	private void run1(){//Threshold Algorithm
