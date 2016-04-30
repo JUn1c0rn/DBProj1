ac4c11a33ef4990f0439153b62a3277dac12db79
JUn1c0rn <JUnicorn@wangjuedeMacBook-Pro.local>
2016-04-30 17:55:12 -0400

    wj

--- src/DBproj1.java
+++ src/DBproj1.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,16 +11,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DBproj1 {
-	private static List init(String fname){//initialize
+	private static List<String> init(String fname){//initialize
 		List<String> dataList=new ArrayList<String>();
-		
 		BufferedReader br=null;
+		System.out.println(fname);
         try { 
-            br = new BufferedReader(new FileReader(fname));
-            String line = "";
-            br.readLine();
+        	File file = new File(fname);
+            br = new BufferedReader(new FileReader(file));
+            String line = null;
+            //br.readLine();
             while ((line = br.readLine()) != null) { 
                 dataList.add(line);
+                //System.out.println(line);
+                //System.out.println(fname);
             }
         }catch (Exception e) {
         }finally{
@@ -52,11 +56,14 @@ public class DBproj1 {
 	
 	public static void main(String[] args){
 		String command = null;
-		int k, n;
-		k = Integer.parseInt(args[0]);
-		n = Integer.parseInt(args[1]);
+		String[] arrtibutes = null;
 		
-		int[][] table;
+		int k, n;
+		//k = Integer.parseInt(args[0]);
+		//n = Integer.parseInt(args[1]);
+		k=5;
+		n=10;
+		int[][] table = null;
 		int[] vector = new int[n];
 		while(true){
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -67,8 +74,44 @@ public class DBproj1 {
 				
 			}
 			String[] commandline = command.split(" ");
+			
+			int row=0, col=0;
 			if (commandline[0].equals("init")) {
-				init(commandline[1]);
+				String filename = "/Users/JUnicorn/Documents/workspace/DBProj1/src/NBA.csv";
+				List<String> dataList = init(filename);
+				if(dataList!=null && !dataList.isEmpty()){
+					//System.out.println(command);
+		            row = dataList.size()-1;
+		            arrtibutes = dataList.get(0).split(",");
+		            col = arrtibutes.length;
+		            table = new int[row][col];
+		            for(int i=0; i<row; i++){
+		            	String[] tuple = dataList.get(i+1).split(",");
+		            	for(int j=0; j<col; j++){
+		            		table[i][j] = Integer.parseInt(tuple[j]);
+		            		//System.out.println(table[i][j]);
+		            	}
+		            }
+		        }
+				
+				Btree<String, String> ptree = new Btree<String, String>();
+				Btree<String, String>[] strees=new Btree[col-1];
+				for(int tt=0; tt<col-1; tt++){
+					strees[tt] = new Btree<String, String>();
+				}
+				for(int i=0; i<row; i++){
+					for(int j=0; j<col; j++){
+						if(j==0){
+							ptree.put(String.valueOf(table[i][j]), String.valueOf(i));
+						}else{
+							strees[j-1].put(String.valueOf(table[i][j]), String.valueOf(table[i][0]));
+						}
+					}
+				}
+				//for(int i=0; i<row; i++){
+					//System.out.println(strees[0].get(String.valueOf(table[i][1])));
+				//}
+				
 			}else if(commandline[0].equals("run1")){
 				
 			}else if(commandline[0].equals("run2")){
