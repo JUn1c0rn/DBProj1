6f8b1d53349b08685cc64f30d6c59caea040649b
JUn1c0rn <JUnicorn@wangjuedeMacBook-Pro.local>
2016-04-30 15:20:02 -0400

    wj

--- src/DBproj1.java
+++ src/DBproj1.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-//
+
 public class DBproj1 {
 	private static void init(String fname){//initialize
 		List<String> dataList=new ArrayList<String>();
